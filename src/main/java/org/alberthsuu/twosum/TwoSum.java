260fe9559896af307ec8731668306bcd79cb73bc
Albert Hsu <iverson52000@gmail.com>
2023-11-24 14:21:36 +0800

    Create the first working unit test

---
+++ src/main/java/org/alberthsuu/twosum/TwoSum.java
@@ -0,0 +1,21 @@
+package org.alberthsuu.twosum;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            if (map.containsKey(num)) {
+                return new int[]{map.get(num), i};
+            } else {
+                map.put(target - num, i);
+            }
+        }
+
+        return new int[2];
+    }
+}
---
+++ src/test/java/org/alberthsuu/twosum/TwoSumTest.java
@@ -0,0 +1,20 @@
+package org.alberthsuu.twosum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+    @Test
+    void twoSumWorks() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] expectedResult = {0, 1};
+
+        TwoSum twoSum = new TwoSum();
+        int[] result = twoSum.twoSum(nums, target);
+
+        assertArrayEquals(result, expectedResult);
+    }
+}
