9347a495886efcffaf617125ad4833f12781fae3
Albert Hsu <iverson52000@gmail.com>
2024-04-16 20:07:19 -0700

    Document the sandard way to create class in C++

--- src/main/cc/org/alberthsuu/hello.cc
+++ src/main/cc/org/alberthsuu/hello.cc
@@ -5,12 +5,35 @@
 using animal::Cat;
 using std::cout;
 using std::endl;
+using std::string;
+
+class Test {
+  int id{3};
+  string name{"Mike"};
+
+ public:
+  Test() = default;
+  // Delete copy constructor
+  Test(const Test& other) = delete;
+  // Delete assignment operator
+  Test& operator=(const Test& other) = delete;
+
+  explicit Test(int id) : id(id) {}
+
+  void print() { cout << id << ": " << name << endl; }
+};
 
 int main() {
   Cat cat;
   cat.speak();
 
   cout << "Hello C++" << endl;
+
+  int value1 = 8;
+  int& value2 = value1;
+  value1 = 20;
+
+  std::cout << value2 << std::endl;
 
   return 0;
 }
